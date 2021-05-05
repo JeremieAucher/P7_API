18e03be8cf0f14c0ce060dc047e5557e46717219
JeremieAucher <jeremie.aucher@gmail.com>
2021-05-05 19:43:56 +0200

    Mise au propre du code

--- app.py
+++ app.py
@@ -111,29 +111,6 @@ def modelPredict(data):
 # def dashboard():
 #     return render_template("dashboard.html")
 
-# @app.route('/api/meteo/')
-# def meteo():
-#     response = requests.get(METEO_API_URL)
-#     content = json.loads(response.content.decode('utf-8'))
-
-#     if response.status_code != 200:
-#         return jsonify({
-#             'status': 'error',
-#             'message': 'La requête à l\'API météo n\'a pas fonctionné. Voici le message renvoyé par l\'API : {}'.format(content['message'])
-#         }), 500
-
-#     data = [] # On initialise une liste vide
-#     for prev in content["list"]:
-#         datetime = prev['dt'] * 1000
-#         temperature = prev['main']['temp'] - 273.15 # Conversion de Kelvin en °c
-#         temperature = round(temperature, 2)
-#         data.append([datetime, temperature])
- 
-#     return jsonify({
-#       'status': 'ok', 
-#       'data': data
-#     })
-
 if __name__ == "__main__":
     # app.run(debug=True)
     app.run()
